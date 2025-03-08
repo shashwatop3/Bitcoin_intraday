34dd19107c3476fe9823d061677c7a3229b6bb5d
shashwatop3 <chaturvedishashwat5@gmail.com>
2025-03-08 11:27:07 +0530

    Update data_fetch.py and add data folder to .gitignore

---
+++ data_fetch.py
@@ -0,0 +1,10 @@
+import gdown
+
+# Define the Google Drive direct download link
+url = 'https://drive.google.com/uc?id=1p6wOzt7PR9onf2f5oo_Q3Vn6g0bosnQL'
+
+# Define the destination path where the file will be saved
+destination = '/Users/Codes/Bitcoin_trader/data/btcusd_15min_preprcd.parquet'
+
+# Download the file from Google Drive
+gdown.download(url, destination, quiet=False)
