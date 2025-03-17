da30ca68d1bc6ebc1b9b8596a2cda1a8d7b11dd2
shashwatop3 <chaturvedishashwat5@gmail.com>
2025-03-17 19:06:23 +0530

    progress

--- order_class.py
+++ order_class.py
@@ -9,6 +9,11 @@ class BuyOrder:
         self.close_price = close_price
         self.pl = self.quantity * (self.price - self.close_price)
         self.status = 'closed'
+    def check(self, current_price):
+        if self.stoploss is not None and current_price <= self.stoploss:
+            self.close(current_price)
+        elif self.takeprofit is not None and current_price >= self.takeprofit:
+            self.close(current_price)
     
 class SellOrder:
     def __init__(self, price, quantity, stoploss=None, takeprofit=None):
@@ -21,3 +26,11 @@ class SellOrder:
         self.close_price = close_price
         self.pl = self.quantity * (self.close_price - self.price)
         self.status = 'closed'
+    def check(self, current_price) -> bool:
+        if self.stoploss is not None and current_price >= self.stoploss:
+            self.close(current_price)
+            return True
+        elif self.takeprofit is not None and current_price <= self.takeprofit:
+            self.close(current_price)
+            return True
+        return False
